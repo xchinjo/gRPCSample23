6511934b2c6e57f556a7facdeea4fa5f2359ba97
xchinjo hank <jongruk.a@hotmail.com>
2023-11-03 20:23:26 +0700

    Initial commit

---
+++ MyApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ServiceStack;
+using MyApp.ServiceModel;
+
+namespace MyApp.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
